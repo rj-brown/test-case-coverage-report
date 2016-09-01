22f8898b9d93f214d17ee5e709abc656d7dfbd56
Claire <csr6ad@virginia.edu>
2016-09-01 18:01:12 +0000

    Created Rally Cardboard app

--- App.js
+++ App.js
@@ -1,9 +1,54 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        //Write app code here
 
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
-    }
+    launch: function() {
+        this.iterationCombobox = this.add({
+            xtype: 'rallyiterationcombobox',
+            listeners: {
+                // change: this._onIterationComboboxChanged,
+                ready: this._onIterationComboboxLoad,
+                scope: this
+            }
+        });
+    },
+
+    _onIterationComboboxLoad: function() {
+        var addNewConfig = {
+            xtype: 'rallyaddnew',
+            recordTypes: ['User Story', 'Defect'],
+            ignoredRequiredFields: ['Name', 'ScheduleState', 'Project'],
+            showAddWithDetails: false,
+            listeners: {
+                beforecreate: this._onBeforeCreate,
+                scope: this
+            }
+        };
+
+        this.addNew = this.add(addNewConfig);
+
+        var cardBoardConfig = {
+            xtype: 'rallycardboard',
+            types: ['Defect', 'User Story'],
+            attribute: 'ScheduleState',
+            storeConfig: {
+                filters: [this.iterationCombobox.getQueryFromSelected()]
+            }
+        };
+        this.cardBoard = this.add(cardBoardConfig);       
+    },
+
+    _onBeforeCreate: function(addNewComponent, record) {
+        record.set('Iteration', this.iterationCombobox.getValue());
+    },
+
+    // _onIterationComboboxChanged: function() {
+    //     var config = {
+    //         storeConfig: {
+    //             filters: [this.iterationCombobox.getQueryFromSelected()]
+    //         }
+    //     };
+
+    //     this.cardBoard.refresh(config);
+    // }
 });
